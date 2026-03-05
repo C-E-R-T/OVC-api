474a8736445402aeb7b9ba9a343b6cbb03741f61
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-05 16:23:52 +0900

    백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/example/ovcbackend/OvcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ovcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ovcbackend/OvcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ovcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
