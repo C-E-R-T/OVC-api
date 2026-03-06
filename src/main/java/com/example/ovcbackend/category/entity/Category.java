aac06de091a2d02911e5bbf787fbdac24b99cc6d
hyeonpark-5 <sorkgus@gmail.com>
2026-03-06 11:47:45 +0900

    feat: createdate 어노테이션 설정

--- src/main/java/com/example/ovcbackend/category/entity/Category.java
+++ src/main/java/com/example/ovcbackend/category/entity/Category.java
@@ -4,6 +4,7 @@ package com.example.ovcbackend.category.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
