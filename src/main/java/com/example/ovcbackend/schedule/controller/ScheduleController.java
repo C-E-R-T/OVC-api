ab7a8f8cd33d1064f9c956e546261b3db035096c
hyeonpark-5 <sorkgus@gmail.com>
2026-03-06 16:23:25 +0900

    feat: swagger 설정

--- src/main/java/com/example/ovcbackend/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/ovcbackend/schedule/controller/ScheduleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/calender")
+@RequestMapping("/api/calendar")
 @RequiredArgsConstructor
 public class ScheduleController {
     private final ScheduleService scheduleService;
--- src/main/java/com/example/ovcbackend/schedule/dto/CalenderResponse.java
+++ src/main/java/com/example/ovcbackend/schedule/dto/CalenderResponse.java
@@ -7,22 +7,22 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-//@Schema(description = "캘린더 반환 dto")
+@Schema(description = "캘린더 반환 dto")
 @Getter
 @Builder
 public class CalenderResponse {
-//    @Schema(description = "스케쥴 아이디", example = "true")
+    @Schema(description = "스케쥴 아이디", example = "true")
     private Long scheduleId;
-//    @Schema(description = "자격증 이름", example = "정보처리기사")
+    @Schema(description = "자격증 이름", example = "정보처리기사")
     private String certificateName;
-//    @Schema(description = "시험 회차 이름", example = "2026년 정기 기사 1회")
+    @Schema(description = "시험 회차 이름", example = "2026년 정기 기사 1회")
     private String examName;
-//    @Schema(description = "필기/실기/기타 구분", example = "필기")
+    @Schema(description = "필기/실기/기타 구분", example = "필기")
     private ExamType examType;
-//    @Schema(description = "시험 분류", example = "APPLY")
+    @Schema(description = "시험 분류", example = "APPLY")
     private String eventType;
-//    @Schema(description = "시험 시작일")
+    @Schema(description = "시험 시작일")
     private LocalDateTime startDate;
-//    @Schema(description = "시험 종료일")
+    @Schema(description = "시험 종료일")
     private LocalDateTime endDate;
 }
