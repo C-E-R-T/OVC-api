56018493ea3201dc88c689a3958ce9701965c8a6
hyeonpark-5 <sorkgus@gmail.com>
2026-03-06 11:30:13 +0900

    feat: certificate entity 굴현

---
+++ src/main/java/com/example/ovcbackend/certificate/entity/Certificate.java
@@ -0,0 +1,41 @@
+package com.example.ovcbackend.certificate.entity;
+
+
+import com.example.ovcbackend.category.entity.Category;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "certificates")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Certificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String agency;
+
+    @Column(name = "exam_trend", columnDefinition = "TEXT")
+    private String examTrend;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
