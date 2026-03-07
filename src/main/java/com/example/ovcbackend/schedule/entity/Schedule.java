1512a95c32fb2db1d9348cb0e606a75ef09c3981
hyeonpark-5 <sorkgus@gmail.com>
2026-03-07 19:50:48 +0900

    fix: schedule entity basetime entity 적용 및 수정

--- src/main/java/com/example/ovcbackend/schedule/entity/Schedule.java
+++ src/main/java/com/example/ovcbackend/schedule/entity/Schedule.java
@@ -1,10 +1,10 @@
 package com.example.ovcbackend.schedule.entity;
 
 import com.example.ovcbackend.certificate.entity.Certificate;
+import com.example.ovcbackend.global.entity.BaseTime;
 import com.example.ovcbackend.schedule.ExamType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -16,21 +16,20 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @EntityListeners(AuditingEntityListener.class)
-public class Schedule {
+public class Schedule extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cert_id", nullable = false)
-    private Certificate certificate;
 
     @Column(name = "exam_name", length = 100, nullable = false)
     private String examName;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "exam_type", length = 20)
+    @Column(name = "exam_type", length = 50)
     private ExamType examType;
+
+    @Column(name = "exam_round")
+    private String examRound;
 
     @Column(name="apply_start_at", nullable = false)
     private LocalDateTime applyStartAt;
@@ -44,7 +43,7 @@ public class Schedule {
     @Column(name = "result_at", nullable = false)
     private LocalDateTime resultAt;
 
-    @CreatedDate
-    @Column(name="created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cert_id", nullable = false)
+    private Certificate certificate;
 }
