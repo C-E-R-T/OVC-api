03278ec99c1148733f7050bd92f33ce212c9b80c
hyeonpark-5 <sorkgus@gmail.com>
2026-03-07 19:35:52 +0900

    feat: basetime entity 구현

---
+++ src/main/java/com/example/ovcbackend/global/entity/BaseTime.java
@@ -0,0 +1,21 @@
+package com.example.ovcbackend.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+    // 시간 생성 클래스
+    @CreatedDate
+    @Column(name="created_at", nullable = false, updatable = false,
+        columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+}
