8b27ed4df18fdc9b324f345228d906c80b7c8be5
hyeonpark-5 <sorkgus@gmail.com>
2026-03-11 08:52:24 +0900

    doc: 로그인 관련 swagger 추가

--- src/main/java/com/example/ovcbackend/auth/dto/LoginRequest.java
+++ src/main/java/com/example/ovcbackend/auth/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.example.ovcbackend.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class LoginRequest {
+    @Schema(name = "email", example = "user@example.com")
     private String email;
+    @Schema(name = "password", example = "1234")
     private String password;
 }
--- src/main/java/com/example/ovcbackend/auth/dto/LoginResponse.java
+++ src/main/java/com/example/ovcbackend/auth/dto/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.example.ovcbackend.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,8 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class LoginResponse {
+    @Schema(name = "accessToken", example = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ1c2VyMkBleGFtcGxlLmNvbSIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3NzMxODY2MTEsImV4cCI6MTc3MzE5MDIxMX0.8BptJD5IJpQ9MGqDeHpon6YazGbCKM9c6p-XklHbOM_odWabrGxqaDaU6l6mrg1f")
     private String accessToken;
+    @Schema(name = "refreshToken", example = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ1c2VyMkBleGFtcGxlLmNvbSIsImlhdCI6MTc3MzE4NjYxMSwiZXhwIjoxNzc0Mzk2MjExfQ.GMvO304r6uKE793oATB6jbXFX4lgdvcyteQN3gefRG2Gu2dLxsSHcSzjI2Gzoqij")
     private String refreshToken;
 }
