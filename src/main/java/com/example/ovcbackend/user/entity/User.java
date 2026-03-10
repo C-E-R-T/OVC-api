f7a83ca8a4abfcd1c3d4d2376757d449be4a6404
hyeonpark-5 <sorkgus@gmail.com>
2026-03-10 14:39:41 +0900

    feat: user entity 구현

---
+++ src/main/java/com/example/ovcbackend/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.ovcbackend.user.entity;
+
+import com.example.ovcbackend.global.entity.BaseTime;
+import com.example.ovcbackend.user.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    @Column(nullable = false)
+    private Role role = Role.ROLE_USER;
+}
