810bf9a0301fd7e6f1a43b5325c353bbc660655c
hyeonpark-5 <sorkgus@gmail.com>
2026-03-11 11:59:03 +0900

    docs: swaggerConfig jwt 인증 설정 추가

--- src/main/java/com/example/ovcbackend/certificate/dto/CertResponse.java
+++ src/main/java/com/example/ovcbackend/certificate/dto/CertResponse.java
@@ -1,6 +1,7 @@
 package com.example.ovcbackend.certificate.dto;
 
 import com.example.ovcbackend.certificate.entity.Certificate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +10,11 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class CertResponse {
+    @Schema(name = "자격증 이름", example = "정보처리기사")
     private String name;
+    @Schema(name = "주관기관", example = "한국산업인력공단")
     private String authority;
+    @Schema(name = "필기 응시료", example = "19400")
     private Integer writtenFee;
     private Integer practicalFee;
     private String examTrend;
--- src/main/java/com/example/ovcbackend/global/config/SwaggerConfig.java
+++ src/main/java/com/example/ovcbackend/global/config/SwaggerConfig.java
@@ -3,8 +3,8 @@ package com.example.ovcbackend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import jakarta.annotation.PostConstruct;
-import org.springdoc.core.utils.SpringDocUtils;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,12 +13,36 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        //swagger에는 jwt 인증 설정이 가능함
+
+        String jwtSchemaName = "jwtAuth";
+        // api 요청 시 사용할 보안 요구사항 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemaName);
+
+        // components에 jwt 보안 스키마 추가
+        Components components = new Components().addSecuritySchemes(jwtSchemaName, new SecurityScheme()
+                .name(jwtSchemaName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        // api 정보 설정
         Info info = new Info()
                 .version("v1.0.0")
-                .title("swagger 확인")
-                .description("swagger 확인용 api 명세서 입니다.");
+                .title("ovc 자격증 통합 프로젝트 swagger")
+                .description("ovc 자격증 통합 프로젝트 api 명세서 입니다.");
 
-        return new OpenAPI().addServersItem(new io.swagger.v3.oas.models.servers.Server().url("/"))
-                .components(new Components()).info(info);
+        // 서버 설정 (base URL을 설정)
+        io.swagger.v3.oas.models.servers.Server server =
+                new io.swagger.v3.oas.models.servers.Server().url("/");
+
+        // api 정보
+        OpenAPI openApi = new OpenAPI()
+                .addServersItem(server)
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .info(info);
+
+        return openApi;
     }
 }
