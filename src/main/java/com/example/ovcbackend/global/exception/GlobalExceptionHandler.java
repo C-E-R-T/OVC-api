d9d888e597b763d40996ea072193614a450566d5
hyeonpark-5 <sorkgus@gmail.com>
2026-03-15 14:32:52 +0900

    feat: user 쪽 exception 추가

--- src/main/java/com/example/ovcbackend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ovcbackend/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.example.ovcbackend.auth.exception.AuthNotFoundException;
 import com.example.ovcbackend.auth.exception.TokenInvalidException;
 import com.example.ovcbackend.certificate.exception.CertNotFoundException;
 import com.example.ovcbackend.schedule.exception.ScheduleNotFoundException;
+import com.example.ovcbackend.user.exception.UserConflictException;
+import com.example.ovcbackend.user.exception.UserNotFoundException;
 import com.example.ovcbackend.user.favorite.exception.FavoriteBadRequestException;
 import com.example.ovcbackend.user.favorite.exception.FavoriteConflictException;
 import com.example.ovcbackend.user.favorite.exception.FavoriteNotFoundException;
@@ -86,6 +88,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ScheduleNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleScheduleNotFound(ScheduleNotFoundException e, HttpServletRequest request) {
         return  buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage(), request);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFound(UserNotFoundException e, HttpServletRequest request) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage(), request);
+    }
+
+    @ExceptionHandler(UserConflictException.class)
+    public ResponseEntity<Map<String, Object>> handleUserConflict(UserConflictException e, HttpServletRequest request) {
+        return  buildErrorResponse(HttpStatus.CONFLICT, e.getMessage(),request);
     }
 
     private ResponseEntity<Map<String, Object>> buildErrorResponse(HttpStatus status, String message,
